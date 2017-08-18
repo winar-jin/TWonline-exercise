5c25549b80592e49aa7d66a1888262ef9732a2d0
winar-jin <winar1994@outlook.com>
2017-08-18 16:13:28 +0800

    update test spec file

--- spec/8-18-findValIndex-spec.js
+++ spec/8-18-findValIndex-spec.js
@@ -14,14 +14,14 @@ describe('在数组中，查找值与item相等的元素出现的所有位置。
     it("为[11, 2, 3, 5, 1]中元素1出现的位置为：-1", () => {
         const result = -1;
         const origin = [11, 2, 3, 5, 1];
-        console.log(`[11, 2, 3, 5, 1]中元素1出现的位置为：-1`);
+        console.log(`[11, 2, 3, 5, 1]中元素10出现的位置为：-1`);
         expect(findValIndex(origin, 10)).toEqual(result);
     });
 
     it("为[1, 2, 3, 4, 5]中的每个元素求二次方。", () => {
         const result = [3];
         const origin = [1, 2, 3, 4, 5];
-        console.log(`[1, 2, 3, 4, 5]元素1出现的位置为：[3]`);
+        console.log(`[1, 2, 3, 4, 5]元素4出现的位置为：[3]`);
         expect(findValIndex(origin, 4)).toEqual(result);
     });
 });
